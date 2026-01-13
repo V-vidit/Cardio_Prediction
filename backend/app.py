d2e734482bace7139ba375ae3fadac11f05ded8f
V-vidit <viditkhant@gmail.com>
2026-01-13 16:30:38 +0530

    First

--- backend/app.py
+++ backend/app.py
@@ -221,5 +221,4 @@ def get_features():
 # ------------------------------------------------------------------
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-
     app.run(host='0.0.0.0', port=port)
